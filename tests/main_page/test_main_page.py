e0c6124d25b58f522510a8609465df996c3af760
Razbibul <bimmer02@yandex.ru>
2023-03-06 13:33:23 +0500

    new

---
+++ tests/main_page/test_main_page.py
@@ -0,0 +1,7 @@
+import pytest
+
+
+def test_work_with_main_page(main_page_class_scope):
+    #main_page_class_scope.open_filter()
+    main_page_class_scope.find_and_open_ract_study('Test2')
+    pass
